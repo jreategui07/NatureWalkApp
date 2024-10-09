7381f1e87c73e36f65a0dfefb61fbd2a56695487
Jonathan Reátegui <j.reategui2012@gmail.com>
2024-10-09 19:39:40 -0400

    Initial commit. Base project.

---
+++ Group05_Nature-Walk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Group05_Nature-Walk
+//
+//  Created by Jonathan Reátegui on 2024-10-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Group05_Nature-Walk/Group05_Nature_WalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Group05_Nature_WalkApp.swift
+//  Group05_Nature-Walk
+//
+//  Created by Jonathan Reátegui on 2024-10-09.
+//
+
+import SwiftUI
+
+@main
+struct Group05_Nature_WalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
