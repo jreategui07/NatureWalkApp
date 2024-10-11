978c58ae7b3087d2bf36f85f983721506f6cfc5d
Jonathan Reátegui <j.reategui2012@gmail.com>
2024-10-11 12:06:50 -0400

    Removing onDelete method from SessionListScreen

--- Group05_Nature-Walk/Views/SessionsListScreen.swift
+++ Group05_Nature-Walk/Views/SessionsListScreen.swift
@@ -28,7 +28,6 @@ struct SessionsListScreen: View {
                             }
                         }
                     }
-                    .onDelete(perform: sessionManager.deleteSession)
                 }
             }
             .overlay(
