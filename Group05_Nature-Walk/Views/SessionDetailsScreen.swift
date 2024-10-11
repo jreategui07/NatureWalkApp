02231282d5d18db2f63af64cdb730b5711cd7388
Jonathan Reátegui <j.reategui2012@gmail.com>
2024-10-11 02:04:12 -0400

    Creating SessionDetailsScreen

---
+++ Group05_Nature-Walk/Views/SessionDetailsScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SessionDetailsScreen.swift
+//  Group05_Nature-Walk
+//
+//  Created by Jonathan Reátegui on 2024-10-09.
+//
+
+import SwiftUI
+
+struct SessionDetailsScreen: View {
+    var body: some View {
+        Text("SessionDetailsScreen")
+    }
+}
+
+#Preview {
+    SessionDetailsScreen()
+}
